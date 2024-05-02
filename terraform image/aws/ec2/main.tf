7aa601d2b454e2ccd36e2b9cf02faeece229ea2b
test <test@test.com>
2024-05-02 14:07:07 +0300

    test

--- terraform image/aws/ec2/main.tf
+++ terraform image/aws/ec2/main.tf
@@ -23,11 +23,15 @@ terraform {
   }
 }
 
+provider "aws" {
+    region = "eu-north-1"
+}
+
 data "aws_subnet" "existing_subnet" {
   id = "subnet-0d65da523f29c0040"
 }
 
-resourcce "aws_instance" "ec2_instance" {
+resource "aws_instance" "ec2_instance" {
   ami = var.ami_id
   instance_type = var.instance_type
   subnet_id = data.aws_subnet.existing_subnet.id
